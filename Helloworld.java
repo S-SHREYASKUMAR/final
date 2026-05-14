90133888fe1ff29325e1d596888a202e9789c6fd
SHREYASKUMAR <154454719+S-SHREYASKUMAR@users.noreply.github.com>
2026-05-14 12:35:11 +0530

    Update Helloworld.java

--- Helloworld.java
+++ Helloworld.java
@@ -1,5 +1,5 @@
-public class Helloworld{
-  public static void main(String []args){
-    System.out.println("hello world");
-  }
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
 }
